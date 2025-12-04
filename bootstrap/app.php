e065cf8f7bd2ea154e94eb6ce4f49e75392c7ee6
Ericklopezchelo <ericklopezchelo@gmail.com>
2025-12-04 18:26:23 -0400

    Corrigiendo bootstrap/app.php y registro de middlewares para evitar errores

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 //esta apartado de bootstrap es para registrarar los middlewares para que se puedan usar en las rutas web.php o api.php si no se hace esto no se podran usar los middlewares personalizados que creemos en la carpeta app/Http/Middleware
+
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\BarberoMiddleware;
 use App\Http\Middleware\AdminMiddleware;
-use App\Http\Middleware\CspMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,19 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function ($middleware): void {
-        // Alias para rutas específicas
-        $middleware->alias([
-            'barbero'=> BarberoMiddleware::class,
-            'admin'=> AdminMiddleware::class
-        ]);
-
-        // Middleware global
-        $middleware->global([
-            CspMiddleware::class,
-        ]);
-    })
-    ->withExceptions(function ($exceptions): void {
+    ->withMiddleware(function (Middleware $middleware): void { //esta parte es para registrar los middlewares.
         //
+        $middleware->alias([
+            'barbero'=> BarberoMiddleware::class, // esta parte es para registrar el middleware barbero que se encuentra en app/Http/Middleware/BarberoMiddleware.php.
+            'admin'=> AdminMiddleware::class
+        ]); 
     })
-    ->create();
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
